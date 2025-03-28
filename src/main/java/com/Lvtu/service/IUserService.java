6c04fa290aca9e8b349c14126b688d731ba84ccc
dazai <3546829078@qq.com>
2025-03-28 17:11:04 +0800

    接口文档与user的部分代码

--- src/main/java/com/Lvtu/service/IUserService.java
+++ src/main/java/com/Lvtu/service/IUserService.java
@@ -1,7 +1,11 @@
 package com.Lvtu.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.Lvtu.dto.LoginFormDTO;
+import com.Lvtu.dto.Result;
 import com.Lvtu.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -12,5 +16,13 @@ import com.Lvtu.entity.User;
  * @since 2021-12-22
  */
 public interface IUserService extends IService<User> {
+
+    Result sendCode(String phone, HttpSession session);
+
+    Result login(LoginFormDTO loginForm, HttpSession session);
+
+    Result sign();
+
+    Result signCount();
 
 }
